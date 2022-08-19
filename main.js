5f1d8f85e3f295cdfa2868cf3162b8096b269859
Varg18 <82880502+Varg18@users.noreply.github.com>
2022-08-19 10:42:04 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+import './src/fonts.css';
+import './src/normalize.css';
+import './style.css';
+import './src/button.css';
+import './src/header.css';
+import './src/main.css';
+import './src/advantages.css';
+import './src/video.css';
+import './src/technology.css';
+import './src/reviews.css';
+import './src/characteristics.css';
+import './src/selection.css';
+import './src/footer.css';
+
+
+import './assets/src/logo.svg';
+import './assets/src/phone.svg';
